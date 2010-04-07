7757b080284306600a9a2949e8a617af5ec86fb0
Craig T Mackenzie and Niko Felger <developers+craig+niko@songkick.com>
2010-04-07 15:29:35 +0100

    Added :sleep option

--- lib/retryable.rb
+++ lib/retryable.rb
@@ -2,6 +2,7 @@ module Retryable
   def retryable(options = {}, &block)
     opts = { :tries     => 1, 
              :on        => StandardError,
+             :sleep     => 0,
              :matching  => /.*/ }.merge(options)
 
     return nil if opts[:tries] == 0
@@ -9,12 +10,17 @@ module Retryable
     retry_exception = [opts[:on]].flatten
     tries           = opts[:tries]
     message_pattern = opts[:matching]
+    sleep_time      = opts[:sleep]
  
     begin
       return yield
     rescue *retry_exception => exception
       raise unless exception.message =~ message_pattern
-      retry if (tries -= 1) > 0
+
+      if (tries -= 1) > 0
+        sleep sleep_time
+        retry
+      end
     end
  
     yield
