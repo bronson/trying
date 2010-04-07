8e55bfadedcdc7c087a79541a5fc4b986f7ad0d7
Craig T Mackenzie and Niko Felger <developers+craig+niko@songkick.com>
2010-04-07 15:27:18 +0100

    Added :matching option

--- lib/retryable.rb
+++ lib/retryable.rb
@@ -1,14 +1,19 @@
 module Retryable
-  def retryable( options = {}, &block )
-    opts = { :tries => 1, :on => StandardError }.merge(options)
+  def retryable(options = {}, &block)
+    opts = { :tries     => 1, 
+             :on        => StandardError,
+             :matching  => /.*/ }.merge(options)
 
     return nil if opts[:tries] == 0
   
-    retry_exception, tries = [ opts[:on] ].flatten, opts[:tries]
+    retry_exception = [opts[:on]].flatten
+    tries           = opts[:tries]
+    message_pattern = opts[:matching]
  
     begin
       return yield
-    rescue *retry_exception
+    rescue *retry_exception => exception
+      raise unless exception.message =~ message_pattern
       retry if (tries -= 1) > 0
     end
  
