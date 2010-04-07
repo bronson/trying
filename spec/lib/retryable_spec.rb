6d70f305d0789d8dd04f67d2971377bf32211606
Niko Felger <niko@songkick.com>
2010-04-07 13:19:27 +0100

    Defaulting to catch StandardError, in order not to catch OutOfMemoryErrors and the like

--- spec/lib/retryable_spec.rb
+++ spec/lib/retryable_spec.rb
@@ -11,7 +11,7 @@ describe "Retryable#retryable" do
     num_calls = 0
     ret_val = retryable do
       num_calls += 1
-      raise Exception if num_calls == 1 # Raise error only the 1st time.
+      raise StandardError if num_calls == 1 # Raise error only the 1st time.
       'foo'
     end
 
@@ -19,7 +19,19 @@ describe "Retryable#retryable" do
     ret_val.should == 'foo'
   end
 
-  it "uses default options of :tries => 1 and :on => Exception when none is given" do
+  it "uses default options of :tries => 1 and :on => StandardError when none is given" do
+    num_calls = 0
+    lambda {
+      retryable do
+        num_calls += 1
+        raise StandardError
+      end
+    }.should raise_error(StandardError)
+
+    num_calls.should == 2
+  end
+  
+  it "should not catch Exceptions by default" do
     num_calls = 0
     lambda {
       retryable do
@@ -28,7 +40,7 @@ describe "Retryable#retryable" do
       end
     }.should raise_error(Exception)
 
-    num_calls.should == 2
+    num_calls.should == 1
   end
 
   it "does not retry if none of the retry conditions occur" do
@@ -41,7 +53,7 @@ describe "Retryable#retryable" do
   it "uses retries :tries times when the exception to retry on occurs every time" do
     num_calls = 0
     lambda {
-      retryable(:tries => 3, :on => StandardError) do
+      retryable(:tries => 3) do
         num_calls += 1
         raise StandardError
       end
@@ -50,16 +62,27 @@ describe "Retryable#retryable" do
     num_calls.should == 4
   end
 
-  it "should respect exception hierarchies (i.e. catch any subclass exceptions)" do
+  it "should catch any subclass exceptions" do
     num_calls = 0
     lambda {
       retryable(:on => StandardError) do
         num_calls += 1
         raise IOError if num_calls == 1 # Raise error only the 1st time.
       end
-
     }.should_not raise_error(IOError)
 
     num_calls.should == 2
+  end
+
+  it "should not catch any superclass exceptions" do
+    num_calls = 0
+    lambda {
+      retryable(:on => StandardError) do
+        num_calls += 1
+        raise Exception if num_calls == 1 # Raise error only the 1st time.
+      end
+    }.should raise_error(Exception)
+
+    num_calls.should == 1
   end
 end
